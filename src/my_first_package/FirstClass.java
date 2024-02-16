4133d44871f58837c49ad4634e6cdbdaea6dbb11
Rabee <eng_hard80@yahoo.com>
2024-02-16 20:36:15 +0000

    First class updated

--- src/my_first_package/FirstClass.java
+++ src/my_first_package/FirstClass.java
@@ -3,5 +3,7 @@ package my_first_package;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+
+        System.out.println("Rabee changed the class");
     }
 }
